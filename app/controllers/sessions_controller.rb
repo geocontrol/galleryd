25315cc7c2af8e0e887e6ec854ee86a70c9f3dce
Mark Simpkins <mark@geekyoto.com>
2012-06-24 22:33:32 +0100

    trying some stuff on user logins.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,26 +2,40 @@ class SessionsController < ApplicationController
   def new
   end
 
-  def create
-    auth = request.env['omniauth.auth']
- # auth = User.from_omniauth(env["omniauth.auth"])
-#    render :text => auth.to_yaml
-
-    unless @auth = Authorization.find_from_hash(auth)
-      @auth = Authorization.create_from_hash(auth, current_user)
-    end
-    
-    self.current_user = @auth.user
-    
-    redirect_to galleries_url, notice: "Welcome, #{current_user.name}."
-  end
-
-  def destroy
-    session[:user_id] = nil
-    redirect_to galleries_url, notice: "Signed out!"
-  end
+#  def create  
+#    user = User.from_omniauth(env["omniauth.auth"])  
+#    session[:user_id] = user.id  
+#    redirect_to root_url, notice: "Signed in!"  
+#  end  
   
-  def failure
-    redirect_to galleries_url, alert: "Authentication failed, please try again."
+  def destroy  
+    session[:user_id] = nil  
+    redirect_to root_url, notice: "Signed out!"  
+  end  
+  
+  def failure  
+    redirect_to root_url, alert: "Authentication failed, please try again."  
   end
+
+  def create
+    raise env['omniauth.auth'].to_yaml
+#    auth = request.env['omniauth.auth']
+
+#    unless @auth = Authorization.find_from_hash(auth)
+#      @auth = Authorization.create_from_hash(auth, current_user)
+#    end
+    
+#    self.current_user = @auth.user
+    
+#    redirect_to galleries_url, notice: "Welcome, #{current_user.name}."
+  end
+
+#  def destroy
+#    session[:user_id] = nil
+#    redirect_to galleries_url, notice: "Signed out!"
+#  end
+  
+#  def failure
+#    redirect_to galleries_url, alert: "Authentication failed, please try again."
+#  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,18 @@ class User < ActiveRecord::Base
   
   has_many :show_relationships, foreign_key: "follower_id", dependent: :destroy
 
+  def self.from_omniauth(auth)  
+ #   find_by_provider_and_uid(auth['provider'], auth['uid']) || create_with_omniauth(auth) 
+    find_by_provider_and_uid(auth['provider'], auth['uid']) || create_with_omniauth(auth) 
+  end  
 
+    def self.create_with_omniauth(auth)  
+      create! do |user|  
+        user.provider = auth["provider"]  
+        user.uid = auth["uid"]  
+        user.name = auth["info"]["name"]  
+      end  
+    end
   
   def self.create_from_hash!(hash)
     create(:name => hash['info']['name'])
